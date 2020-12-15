81013543f6ec7006a7acce911b4e9bfc617c0163
totoro686 <bowenhh112@gmail.com>
2020-12-15 00:25:44 -0600

    Create 930. Binary Subarrays With Sum.java

---
+++ 930. Binary Subarrays With Sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numSubarraysWithSum(int[] A, int S) {
+        int n = A.length;
+        int left = 0;
+        int count = 0, sum = 0;
+        for (int right = 0; right < n; right++) {
+            sum += A[right];
+            while (left < right && sum > S) {
+                sum -= A[left];
+                left++;
+            }
+            if (sum == S) count++;
+            for (int i = left; sum == S && i < right && A[i] == 0; i++)
+                count++;
+        }
+
+        return count;
+    }
+}
